58c0bfea7c5a8cfc74552e5da8803cce324451ae
gucheng <gcloudy@qq.com>
2019-09-18 08:39:25 +0800

    轮播组件的基本实现

--- music.163/src/application/Home/index.js
+++ music.163/src/application/Home/index.js
@@ -17,7 +17,7 @@ function Home(props) {
                 <NavLink to="/singers" activeClassName="selected"><TabItem><span>歌手</span></TabItem></NavLink>
                 <NavLink to="/rank" activeClassName="selected"><TabItem><span>排行榜</span></TabItem></NavLink>
             </Tab>
-            { renderRoutes(route.routers) }
+            { renderRoutes(route.router) }
         </div>
     )
 }
--- music.163/src/components/slider/index.js
+++ music.163/src/components/slider/index.js
@@ -9,27 +9,26 @@ function Slider(props) {
     const [sliderSwiper, setSliderSwiper] = useState(null);
     const { bannerList } = props;
 
-
     useEffect(() => {
         if (bannerList.length && !sliderSwiper) {
             let sliderSwiper = new Swiper('.slider-container', {
                 loop: true,
                 autoplay: true,
                 autoplayDisableOnInteraction: false,
-                pagination: {el: './swiper-pagination'},
+                pagination: {el: '.swiper-pagination'},
             });
             setSliderSwiper(sliderSwiper)
         }
     }, [bannerList.length, sliderSwiper]);
-
     return (
         <SliderContainer>
+            <div className="before"></div>
             <div className="slider-container">
                 <div className="swiper-wrapper">
                     {
                         bannerList.map(slider => {
                             return (
-                                <div classNmae="swiper-slider" key={slider.imageUrl}>
+                                <div className="swiper-slider" key={slider.imageUrl}>
                                     <div className="slider-nav">
                                         <img src={slider.imageUrl} width="100%" alt="推荐" />
                                     </div>
