495622df94f0379266d0e16788e8d78fe86f6d77
gucheng <gcloudy@qq.com>
2019-09-23 08:31:02 +0800

    数据层开发基本 需确认代理

--- music.163/src/application/Recommend/store/actionCreators.js
+++ music.163/src/application/Recommend/store/actionCreators.js
@@ -16,6 +16,20 @@ export const changeRecommendList = (data) => ({
 
 export const getBannerList = () => {
     return (dispatch) => {
-        
+        getBannerRequest().then(data => {
+            dispatch(changeBannerList(data.banners));
+        }).catch(() => {
+            console.log('轮播图数据传输错误')
+        })
+    }
+}
+
+export const getRecommendList = () => {
+    return (dispatch) => {
+        getRecommendListRequest().then(data => {
+            dispatch(changeRecommendList(data.result));
+        }).catch(() => {
+            console.log('推荐歌单数据传输错误')
+        })
     }
 }
