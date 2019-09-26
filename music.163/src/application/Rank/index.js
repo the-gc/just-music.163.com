3a21014779a3cb47ef36b1cdf9a9e89e035780c5
gucheng <gcloudy@qq.com>
2019-09-26 23:57:05 +0800

    目录修改

--- music.163/src/application/Rank/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Rank(props) {
-    return (
-        <div>Rank</div>
-    )
-}
-
-export default React.memo(Rank)
--- music.163/src/store/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux-immutable'
-import { reducer as recommendReducer } from "../application/Recommend/store/index";
-import { reducer as singerReducer } from "../application/Singers/store/index";
-
-export default combineReducers({
-    recommend: recommendReducer,
-    singers: singerReducer,
-})
