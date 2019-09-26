4ddbe364beba0909fb7aefa820857aa3512b462c
gucheng <gcloudy@qq.com>
2019-09-26 08:29:27 +0800

    歌手模块 reducer 连接组件

--- music.163/src/application/Singers/index.js
+++ music.163/src/application/Singers/index.js
@@ -1,47 +1,66 @@
 import React, { useState } from 'react'
+import { connect } from "react-redux";
 import Scroll from '../../components/Scroll'
 import Horizen from "../../baseUI/horizen-item";
 import { categoryTypes, alphaTypes} from "../../api/config";
 import { NavContainer, List, ListItem, ListContainer} from './style'
+import { 
+    getSingerList,
+    getHotSingerList,
+    changeEnterLoading,
+    changePageCount,
+    refreshMoreSingerList,
+    changePullDownLoading,
+    changePullUpLoading,
+    refreshMoreHotSingerList,
+ } from "./store/actionCreators";
+
 
 // mock数据
-const singerList = [1, 2,3, 4,5,6,7,8,9,10,11,12].map(item => {
-    return {
-      picUrl: "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
-      name: "隔壁老樊",
-      accountId: 277313426,
-    }
-});
+// const singerList = [1, 2,3, 4,5,6,7,8,9,10,11,12].map(item => {
+//     return {
+//       picUrl: "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
+//       name: "隔壁老樊",
+//       accountId: 277313426,
+//     }
+// });
 
-const renderSingerList = () => {
-    return (
-        <List>
-            {
-                singerList.map((item, index) => {
-                    return (
-                        <ListItem key={item.accountId + ' ' + index}>
-                            <div className="img_wrapper">
-                                <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music" />
-                            </div>
-                            <span className="name">{item.name}</span>
-                        </ListItem>
-                    )
-                })
-            }
-        </List>
-    )
-}
+
  
 function Singer(props) {
+    const { singerList, pageCount, pullUpLoading, pullDownLoading, enterLoading} = props;
+    const {updateDispatch} = props;
     let [category, setCategory] = useState('');
     let [alpha, setAlpha] = useState('');
-
     let handleUpdateAlpha = (val) => {
-        setAlpha(val)
+        setAlpha(val);
+        updateDispatch(category, val);
     }
     let handleUpdateCategory = (val) => {
-        setCategory(val)
+        setCategory(val);
+        updateDispatch(val, alpha);
     }
+
+    const renderSingerList = () => {
+        const {singerList} = props;
+        return (
+            <List>
+                {
+                    singerList.map((item, index) => {
+                        return (
+                            <ListItem key={item.accountId + ' ' + index}>
+                                <div className="img_wrapper">
+                                    <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music" />
+                                </div>
+                                <span className="name">{item.name}</span>
+                            </ListItem>
+                        )
+                    })
+                }
+            </List>
+        )
+    }
+
     return (
         <div>
             <NavContainer>
@@ -65,4 +84,46 @@ function Singer(props) {
         
     )
 }
-export default React.memo(Singer)
+
+const mapStateTopProps = (state) => ({
+    singerList: state.getIn(['singers', 'singerList']),
+    enterLoading: state.getIn(['singers', 'enterLoading']),
+    pullUpLoading: state.getIn(['singers', 'pullUpLoading']),
+    pullDownLoading: state.getIn(['singers', 'pullDownLoading']),
+    pageCount: state.getIn(['singers', 'pageCount'])
+});
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getHotSingerDispatch() {
+            dispatch(getHotSingerList())
+        },
+        updateDispatch(category, alpha) {
+            dispatch(changePageCount(0)); // 改变了分类 pageCount 清零
+            dispatch(changeEnterLoading(true));
+            dispatch(getSingerList(category, alpha));
+        },
+        //  滑到最底端刷新部分的处理
+        pullUpRefreshDispatch(category, alpha, hot, count) {
+            dispatch(changePullUpLoading(true));
+            dispatch(changePageCount(count+1));
+            if(hot) {
+                dispatch(refreshMoreHotSingerList());
+            }else {
+                dispatch(refreshMoreSingerList(category, alpha))
+            }
+        },
+        // 顶部下拉刷新
+        pullDownRefreshDispatch(category, alpha) {
+            dispatch(changePullDownLoading(true));
+            dispatch(changePageCount(0)); // 重新获取数据 pageCount 清零
+            if (category === '' && alpha === '') {
+                dispatch(getHotSingerList());
+            }else {
+                dispatch(getSingerList(category, alpha))
+            }
+        }
+    }
+}
+
+export default connect(mapStateTopProps, mapDispatchToProps)(Singer)
--- music.163/src/store/reducer.js
+++ music.163/src/store/reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux-immutable'
 import { reducer as recommendReducer } from "../application/Recommend/store/index";
+import { reducer as singerReducer } from "../application/Singers/store/index";
 
 export default combineReducers({
-    recommend: recommendReducer
+    recommend: recommendReducer,
+    singers: singerReducer,
 })
