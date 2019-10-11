a7a0adee7ca9e00361edd994b36689263f447ab5
gucheng <gcloudy@qq.com>
2019-10-11 08:34:38 +0800

    rank部分 redux开发

--- src/api/request.js
+++ src/api/request.js
@@ -16,3 +16,8 @@ export const getHotSingerListRequest = (count) => {
 export const getSingerListRequest = (category, alpha, count) => {
     return axiosInstance.get(`/artist/list?cat=${category}&initial=${alpha.toLowerCase()}&offset=${count}`);
 }
+
+
+export const getRankListRequest = () => {
+    return axiosInstance.get('/toplist/detail');
+}
--- src/application/Rank/index.js
+++ src/application/Rank/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { connect } from "react-redux";
+import { getRankList } from './store';
 
 function Rank(props) {
     return (
@@ -6,4 +8,17 @@ function Rank(props) {
     )
 }
 
-export default React.memo(Rank)
+
+const mapStateToProps = (state) => ({
+    rankList: state.getIn(['rank', 'rankList']),
+    loading: state.getIn(['rank', 'loading'])
+});
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getRankListDataDispatch() {
+            dispatch(getRankList());
+        }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Rank));
