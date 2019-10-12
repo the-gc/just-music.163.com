bd4f0cef67df1b49955f271e5d9a46adf9de7ca6
gucheng <gcloudy@qq.com>
2019-10-12 08:33:56 +0800

    处理rankList 数据

--- src/application/Rank/index.js
+++ src/application/Rank/index.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React , { useEffect} from 'react'
 import { connect } from "react-redux";
 import { getRankList } from './store';
+import { filterIndex } from "../../api/utils";
+
 
 function Rank(props) {
+    const {rankList, loading} = props;
+    const {getRankListDataDispatch} = props;
+
+    useEffect(() => {
+        getRankListDataDispatch();
+    }, []);
+
+    let globalStartIndex = filterIndex(rankList);
+    let officialList = rankList.slice(0, globalStartIndex);
+    let globalList = rankList.slice(globalStartIndex);
     return (
         <div>Rank</div>
     )
