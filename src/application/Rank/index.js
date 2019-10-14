49283bab96d9579de38bfd3e0b400bcf6cac8bc8
gucheng <gcloudy@qq.com>
2019-10-14 08:40:15 +0800

    rank 页基本结构

--- src/application/Rank/index.js
+++ src/application/Rank/index.js
@@ -1,12 +1,17 @@
 import React , { useEffect} from 'react'
-import { connect } from "react-redux";
-import { getRankList } from './store';
-import { filterIndex } from "../../api/utils";
+import { connect } from "react-redux"
+import { getRankList } from './store'
+import { filterIndex } from "../../api/utils"
+import Scroll from '../../components/Scroll'
+import { List, ListItem, SongList, Container} from './style'
 
 
 function Rank(props) {
-    const {rankList, loading} = props;
+    const {rankList: list, loading} = props;
     const {getRankListDataDispatch} = props;
+
+    let rankList = list ? list.toJS() : [];
+
 
     useEffect(() => {
         getRankListDataDispatch();
@@ -15,8 +20,57 @@ function Rank(props) {
     let globalStartIndex = filterIndex(rankList);
     let officialList = rankList.slice(0, globalStartIndex);
     let globalList = rankList.slice(globalStartIndex);
+
+
+    const renderRankList = (list, global) => {
+        return (
+            <List globalRank={global}>
+                {
+                    list.map((item) => {
+                        return (
+                            <ListItem 
+                              key={item.coverImgId} 
+                              tracks={item.tracks}
+                              onClick={() => {}}>
+                                <div className="img_wrapper">
+                                    <img src={item.coverImgUrl} alt=""/>
+                                    <div className="decorate"></div>
+                                    <span className="update_frequecy">{item.updateFrequency}</span>
+                                </div>
+                                
+                            </ListItem>
+                        )
+                    })
+                }
+            </List>
+        )
+    }
+
+    const renderSongList = (list) => {
+        return list.length ? (
+            <SongList>
+                {
+                    list.map((item, index) => {
+                        return <li key={index}>{index+1}, {item.first} - {item.second}</li>
+                    })
+                }
+            </SongList>
+        ) : null
+    }
+
+    // 榜单数据未加载出来之前都给隐蔽
+    let displayStyle = loading ? {"display": 'none'} : {"display" : ""};
     return (
-        <div>Rank</div>
+        <Container>
+            <Scroll>
+                <div>
+                    <h1 className="offical" style={displayStyle}>官方榜</h1>
+                    { renderRankList(officialList) } 
+                    <h1 className="global" style={displayStyle}>全球榜</h1>
+                    { renderRankList(globalList, true) }
+                </div>
+            </Scroll>
+        </Container>
     )
 }
 
