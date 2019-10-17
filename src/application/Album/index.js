f85d70e5cd132294a47b3abc1c94e37af240dcab
gucheng <gcloudy@qq.com>
2019-10-17 08:10:12 +0800

    歌单详情路由跳转

---
+++ src/application/Album/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Container } from "./style"
+
+function Album(props) {
+    return (
+        <Container></Container>
+    )
+}
+
+export default Album
--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import LazyLoad from 'react-lazyload'
+import { withRouter } from 'react-router-dom'
 import {
     ListWrapper,
     ListItem,
@@ -8,6 +9,9 @@ import {
 import { getCount } from '../../api/utils'
 
 function RecommendList(props) {
+    const enterDetail = (id) => {
+        props.history.push(`/recommend/${id}`);
+    }
     return (
         <ListWrapper>
             <h1 className="title">推荐歌单</h1>
@@ -15,7 +19,7 @@ function RecommendList(props) {
                 {
                     props.recommendList.map((item, index) => {
                         return (
-                            <ListItem key={item.id + index}>
+                            <ListItem key={item.id} onClick={() => {enterDetail(item.id)}}>
                                 <div className="img_wrapper">
                                     <div className="decorate"></div>
                                     <LazyLoad placeholder={<img width="100%" height="100%" src={require('./music.png')} alt="music"/>}>
@@ -39,4 +43,4 @@ function RecommendList(props) {
 }
 
 
-export default React.memo(RecommendList)
+export default React.memo(withRouter(RecommendList))
